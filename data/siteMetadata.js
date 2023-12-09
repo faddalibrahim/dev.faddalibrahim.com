d845be1ff167c5f243e58a909ddf3a10d6cf23ce
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-12-09 07:27:29 +0000

    feat: content for about page

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Faddal Ibrahim',
+  author: 'Faddal Ibrahim',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Exploring my life\'s rectangle -- Programming, Design, Writing, Mathematics',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://dev.faddalibrahim.com',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/faddalibrahim',
+  twitter: 'https://twitter.com/FaddalIbrahim',
+  linkedin: 'https://www.linkedin.com/in/faddalibrahim',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
